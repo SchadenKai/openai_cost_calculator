a24317f83dfe5011ef1e62d8de21aaf66cdb77bf
SchadenKai <kairustecsonpersonal@gmail.com>
2026-01-18 17:09:22 +0800

    test: add test for smaller non divisible by tens value

--- openai_cost_calculator/openai_cost_calculator_test.py
+++ openai_cost_calculator/openai_cost_calculator_test.py
@@ -7,10 +7,24 @@ class TestOpenAICostCalculator(unittest.TestCase):
 
     def test_calculate_cost(self):
         # Test the cost calculation for a specific model
-        result = calculate_cost(model_name='gpt-4-0125-preview', input_tokens=1000000, output_tokens=500000)
-        self.assertEqual(result['input_cost'], Decimal('10.00'))
-        self.assertEqual(result['output_cost'], Decimal('15.00'))
-        self.assertEqual(result['total_cost'], Decimal('25.00'))
+        result = calculate_cost(
+            model_name="gpt-4-0125-preview", input_tokens=1000000, output_tokens=500000
+        )
+        self.assertEqual(result["input_cost"], Decimal("10.00"))
+        self.assertEqual(result["output_cost"], Decimal("15.00"))
+        self.assertEqual(result["total_cost"], Decimal("25.00"))
+
+    def test_with_low_token_count(self):
+        result = calculate_cost(
+            model_name="gpt-4-0125-preview", input_tokens=512, output_tokens=512
+        )
+        self.assertEqual(result["input_cost"], Decimal("0.00512"))
+        self.assertEqual(result["output_cost"], Decimal("0.01536"))
+        self.assertEqual(result["total_cost"], Decimal("0.02048"))
+
+    def test_with_embedding_model(self):
+        result = calculate_cost(model_name="text-embedding-3-small", input_tokens=512)
+        self.assertEqual(result["input_cost"], Decimal("0.00001024"))
 
     def test_model_name_none(self):
         # Test that the function raises an error if the model name is not specified
