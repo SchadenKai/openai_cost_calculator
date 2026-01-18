fdbb9e60056241406d52d75a33197b96822625b5
SchadenKai <kairustecsonpersonal@gmail.com>
2026-01-18 17:09:59 +0800

    feat: add support for text embedding (currently not including cost for batches)

--- openai_cost_calculator/openai_cost_calculator_test.py
+++ openai_cost_calculator/openai_cost_calculator_test.py
@@ -3,8 +3,8 @@ import unittest
 from openai_cost_calculator.openai_cost_calculator import calculate_cost
 from openai_cost_calculator.pricing_information import model_pricing
 
-class TestOpenAICostCalculator(unittest.TestCase):
 
+class TestOpenAICostCalculator(unittest.TestCase):
     def test_calculate_cost(self):
         # Test the cost calculation for a specific model
         result = calculate_cost(
@@ -34,15 +34,22 @@ class TestOpenAICostCalculator(unittest.TestCase):
     def test_invalid_model_name(self):
         # Test that the function raises an error for an invalid model name
         with self.assertRaises(ValueError):
-            calculate_cost(model_name='invalid-model', input_tokens=1000, output_tokens=1000)
+            calculate_cost(
+                model_name="invalid-model", input_tokens=1000, output_tokens=1000
+            )
 
     def test_model_prices(self):
-        self.assertEqual(model_pricing['gpt-4-0125-preview']['input_price'], 10.00)
-        self.assertEqual(model_pricing['gpt-4-0125-preview']['output_price'], 30.00)
-        self.assertEqual(model_pricing['gpt-4-1106-preview']['input_price'], 10.00)
-        self.assertEqual(model_pricing['gpt-4-1106-preview']['output_price'], 30.00)
-        self.assertEqual(model_pricing['gpt-4-1106-vision-preview']['input_price'], 10.00)
-        self.assertEqual(model_pricing['gpt-4-1106-vision-preview']['output_price'], 30.00)
+        self.assertEqual(model_pricing["gpt-4-0125-preview"]["input_price"], 10.00)
+        self.assertEqual(model_pricing["gpt-4-0125-preview"]["output_price"], 30.00)
+        self.assertEqual(model_pricing["gpt-4-1106-preview"]["input_price"], 10.00)
+        self.assertEqual(model_pricing["gpt-4-1106-preview"]["output_price"], 30.00)
+        self.assertEqual(
+            model_pricing["gpt-4-1106-vision-preview"]["input_price"], 10.00
+        )
+        self.assertEqual(
+            model_pricing["gpt-4-1106-vision-preview"]["output_price"], 30.00
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
